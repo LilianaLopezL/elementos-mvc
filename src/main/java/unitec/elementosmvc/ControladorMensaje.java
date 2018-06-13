160ae0b0bd7f42f21967a8b74c4ce2302c0c013f
T-107 <T-107@PC191.mshome.net>
2018-06-12 21:41:27 -0500

    SERVICIO WEB

---
+++ src/main/java/unitec/elementosmvc/ControladorMensaje.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unitec.elementosmvc;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author T-107
+ */
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+
+public class ControladorMensaje 
+{
+    @GetMapping("/hola")
+    public String algo()
+    {
+        return "Hola mundo desde un controlador"; // ES UN SERVICIO WEB
+    }
+}
