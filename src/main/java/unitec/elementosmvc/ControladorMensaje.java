6457427cca2728d0c4efe15c5728f3c9e883b79e
T-107 <T-107@PC191.mshome.net>
2018-06-15 21:42:54 -0500

    OPERACIONES CRUD

--- src/main/java/unitec/elementosmvc/ControladorMensaje.java
+++ src/main/java/unitec/elementosmvc/ControladorMensaje.java
@@ -5,8 +5,12 @@
  */
 package unitec.elementosmvc;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,9 +25,29 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class ControladorMensaje 
 {
+    /*
     @GetMapping("/hola")
     public String algo()
     {
         return "Hola mundo desde un controlador"; // ES UN SERVICIO WEB
+    }
+*/
+    @Autowired RepositorioMensaje repoMensaje;
+    
+    // Caso a) Buscar Todos
+    
+    @GetMapping("/mensaje")
+    public List<Mensaje> buscarTodos()
+    {
+        return repoMensaje.findAll();
+        
+    }
+    
+    // Caso a) Buscar por ID
+    
+    @GetMapping("/mensaje/{id}")
+    public Mensaje buscarPorId(@PathVariable String id)
+    {
+       return repoMensaje.findById(id).get();
     }
 }
